5651978beaa64da1fa0b13c1d847cb088fff24e7
qholness <qholness@gmail.com>
2016-07-26 17:26:06 -0500

    Changed to origin remote

--- pull_site.py
+++ pull_site.py
@@ -2,5 +2,5 @@ from subprocess import call
 def Call(command):
 	return call("{}".format(command), shell=True)
 	
-Call("git pull ThirdStrike master")
-Call("git reset --hard ThirdStrike/master")
+Call("git pull origin master")
+Call("git reset --hard origin/master")
